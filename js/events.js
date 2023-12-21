8bebedc13d9188e49e1521178fa7530b9bfc4b34
Mohammad Zakir Hossain <engineer.zakir.hossain@gmail.com>
2023-12-21 22:43:40 +0200

    event-listener

--- js/events.js
+++ js/events.js
@@ -10,4 +10,24 @@ const blueButton = document.getElementById('blue-button');
 // Set the name of the function
 blueButton.onclick = createBkBlueColor;
 
+// for Green button with annonymous function
+const greenButton = document.getElementById('green-button');
+greenButton.onclick = function (){
+    document.body.style.backgroundColor = 'green';
+}
+console.log(greenButton);
 
+// pink-button
+function createBkPinkColor(){
+    document.body.style.backgroundColor = 'pink';
+}
+const pinkButton = document.getElementById('pink-button');
+pinkButton.addEventListener("click", createBkPinkColor);
+
+console.log(pinkButton);
+
+// black-button
+const blackButton = document.getElementById('black-button');
+blackButton.addEventListener("click", function(){
+    document.body.style.backgroundColor = 'black';
+});
